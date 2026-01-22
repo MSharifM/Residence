6854c4b7d6f9e652bb6b2c5d3afec96d623e076d
MSharifM <MSharifMdev@gmail.com>
2026-01-22 13:19:44 +0330

    style: Edit question page & Add AboutUs page

--- CofeeShop/Controllers/HomeController.cs
+++ CofeeShop/Controllers/HomeController.cs
@@ -30,6 +30,12 @@ public class HomeController : Controller
         return View();
     }
 
+    [Route("AboutUs")]
+    public IActionResult AboutUs()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
